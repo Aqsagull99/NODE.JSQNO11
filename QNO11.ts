4c4c24ea89be115e416c36c3a86ca79b41a82cc3
Aqsagull99 <159878357+Aqsagull99@users.noreply.github.com>
2024-02-17 00:38:08 +0500

    Add files via upload

---
+++ QNO11.ts
@@ -0,0 +1,9 @@
+//  QNO:11
+// Names: Store the names of a few of your friends in a array called names. Print each person’s name by accessing each element in the list, one at a time.
+
+
+// let student:string[]=["naila","kashaf","qirat","noreen"]
+// console.log(student[0])
+// console.log(student[1])
+// console.log(student[2])
+// console.log(student[3])
